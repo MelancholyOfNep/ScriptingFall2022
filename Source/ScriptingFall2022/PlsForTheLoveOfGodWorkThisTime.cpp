315597b848a994dce3084e1a74347f0cea6b4c37
Nep <49031387+MelancholyOfNep@users.noreply.github.com>
2022-10-13 16:45:45 -0400

    Occasional update

---
+++ Source/ScriptingFall2022/PlsForTheLoveOfGodWorkThisTime.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlsForTheLoveOfGodWorkThisTime.h"
+
+// Sets default values
+APlsForTheLoveOfGodWorkThisTime::APlsForTheLoveOfGodWorkThisTime()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlsForTheLoveOfGodWorkThisTime::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlsForTheLoveOfGodWorkThisTime::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/ScriptingFall2022/TestAgain.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SCRIPTINGFALL2022_API TestAgain
+{
+public:
+	TestAgain();
+	~TestAgain();
+};
